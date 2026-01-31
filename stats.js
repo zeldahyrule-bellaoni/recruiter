ae15c78df0e804b1249b77306ed3089122b7f20e
zeldahyrule-bellaoni <hyrulezelda504@gmail.com>
2026-01-31 13:06:49 +0530

    Update stats.js

--- stats.js
+++ stats.js
@@ -6,8 +6,8 @@ module.exports = async function runStatsExtractor(page) {
   console.log("🚀 Starting Phase 1: Profile ID Extraction (No Club)");
 
   const startPage = 1; //change
-  const endPage = 145; //change
-  const tierId = 6; //change
+  const endPage = 194; //change
+  const tierId = 5; //change
   let allProfiles = [];
 
   await page.goto('https://v3.g.ladypopular.com', { waitUntil: 'domcontentloaded', timeout: 60000 });
